40e67ae018c8dd81012bae6401eb536e4c2d08ac
admin <admin@admins-MBP.fios-router.home>
2021-08-06 17:07:55 -0400

    Created cart-item component and connected dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,11 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id == cartItemToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id
     );
 
     if (existingCartItem) {
         return cartItems.map(cartItem => 
-            cartItem.id == cartItemToAdd.id 
+            cartItem.id === cartItemToAdd.id 
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem
         )
@@ -13,3 +13,4 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [ ...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
